e88287adfd18f9802e8fd2df11357f318d01c411
Alina Cucu <alinabaciu87@gmail.com>
2017-11-26 01:35:14 +0100

    stripped description of original html tags and downloaded the data once again

--- stackoverflowparser_12june.py
+++ stackoverflowparser_12june.py
@@ -1,8 +1,13 @@
 import feedparser
 import sqlite3
+import datetime
+import re
+from bs4 import BeautifulSoup  # Or from BeautifulSoup import BeautifulSoup
 
-conn = sqlite3.connect('/Users/alina/Documents/BerlinStartupJobsAnalysis', timeout=1)
+
+conn = sqlite3.connect('/Users/alina/Desktop/StartupJobs/BerlinStartupJobsAnalysis', timeout=1)
 d = feedparser.parse('https://stackoverflow.com/jobs/feed?location=berlin')
+
 
 
 for entry in d['entries']:
@@ -10,26 +15,20 @@ for entry in d['entries']:
 		continue
 	tags = entry['tags']
 	title = entry['title']
-	description = entry['description']
+	soup = BeautifulSoup(entry['description'])
+	description = soup.get_text()
 	company = entry['author']
+
 	c = conn.cursor()
 	#print(title)
-	c.execute("INSERT INTO Stackoverflow_jobs_12_june(Jobtitle, Description, Company_name) VALUES (?,?,?)",(title, description, company))
+
+	c.execute("INSERT INTO Stackoverflow_jobs_12_june(jobtitle, description, company_name, Date, source) VALUES (?,?,?,?,?)",(title, description, company, datetime.date.today(), 'Stackoverflow'))
 	jobid = c.lastrowid
 
 	for tag in tags:
 		term = tag['term']
-		print(term)
-		c.execute("INSERT INTO Skills_12_june(skill_name, job_id) VALUES (?,?)",(term, jobid))
-
-
-
-	
-#print('\n')
-	# company = entry['author']
-	# summary = entry['summary']
-	# experience_required = False if summary.find('years') == -1 else True
-
+		#print(term)
+		c.execute("INSERT INTO Skills_12_june(skill, job_id) VALUES (?,?)",(term, jobid))
 
 
 
